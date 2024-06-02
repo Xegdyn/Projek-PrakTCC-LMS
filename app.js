a1d76bb12397fffa76be9795b4abd951b3b7fa0a
Nicholas Gendy Putra Mahardika <126652316+Xegdyn@users.noreply.github.com>
2024-06-02 21:03:13 +0700

    upload pengunjung service

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const pengunjungRouter = require("./pengunjung");
+const port = "3200"; // Sesuaikan port jika diperlukan
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/pengunjung", pengunjungRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Pengunjung Service! 😁");
+});
+
+app.listen(port, () => {
+  console.log("Server Connected on PORT: " + port + "/");
+});
