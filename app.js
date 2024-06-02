4a92760ad1e62efe2f9c3d2456857f98cc7cc79a
Nicholas Gendy Putra Mahardika <nicholasgendyputra@gmail.com>
2024-06-02 22:26:37 +0700

    idk port

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 8080; // Use PORT environment variable
-
+const port = 3100;
 const bukuRouter = require("./buku");
 
 // Enable CORS
